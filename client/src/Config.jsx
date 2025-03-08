e74c1b591a4ccb97e33441f3197a3ad1325d4ead
joytianya <zhangxw0413@gmail.com>
2025-03-08 10:58:52 +0800

    准备部署到 Render

--- client/src/Config.jsx
+++ client/src/Config.jsx
@@ -8,13 +8,13 @@ export const modelOptions = [
 export const maxHistoryLength = 10;  // 添加最大对话长度配置
 
 // API 地址
-//export const serverURL = 'https://mini-chatbot-backend.onrender.com';
+export const serverURL = 'https://mini-chatbot-backend.onrender.com';
 //export const serverURL = 'http://localhost:5001';
 //export const serverURL = window.location.//export const server//export const serverURL = 'URL_ADDRESS-chatbot-backend.onrender.com';
-export const serverURL = process.env.NODE_ENV === 'production' 
-  ? 'https://mini-chatbot-backend.onrender.com'
-  : window.location.hostname === 'localhost' 
-    ? 'http://localhost:5001'
-    : `http://${window.location.hostname}:5001`;
+//export const serverURL = process.env.NODE_ENV === 'production' 
+//  ? 'https://mini-chatbot-backend.onrender.com'
+//  : window.location.hostname === 'localhost' 
+//    ? 'http://localhost:5001'
+//    : `http://${window.location.hostname}:5001`;
 
 // 本地调试用 
